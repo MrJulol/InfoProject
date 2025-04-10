d6edc158a35bbdf1446cea59f173051614e1451a
Julian <julian.thaler06@gmail.com>
2025-04-10 12:00:26 +0200

    change to Svelte

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,37 @@
 const app = require("express")();
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const mariadb = require("mariadb");
 
+const PORT = 3500;
 
-const PORT = 3500
+const pool = mariadb.createPool({
+  host: "10.10.31.12",
+  user: "root",
+  password: "tocorddbroot1234",
+  database: "InfoProjekt1",
+  connectionLimit: 5,
+});
 
+app.use(cors());
+app.use(bodyparser.json());
 
 app.get("/", (_, res) => {
-	return res.send("<p>Hello</p>")
-})
+  let connection;
+  try {
+    connection = pool.getConnection();
+    console.log("Connected to DB");
+  } catch (err) {
+    console.error("Error connecting to DB: ", err);
+    return res.status(500).send("Error connecting to DB");
+  } finally {
+    if (connection) {
+      connection.release();
+    }
+  }
+  return res.send("<p>Hello</p>");
+});
 
-app.listen(3500, () => {
-	console.log("Server started")
-})
+app.listen(PORT, () => {
+  console.log("Server started");
+});
