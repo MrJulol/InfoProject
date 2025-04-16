8884eceed5dce07dc2fd7e6b55319db33ff5eb7f
Leo Oberhollenzer <oberhollenzerleo@gmail.com>
2025-04-16 11:12:20 +0200

    IP

--- vite-project/src/routes/+page.svelte
+++ vite-project/src/routes/+page.svelte
@@ -67,7 +67,7 @@ function getCookie(name: string) {
     }
 
     try {
-      const response = await fetch('/users/register', {
+      const response = await fetch('http:loclahost:3500/users/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
